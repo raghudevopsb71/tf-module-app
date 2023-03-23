c2874bce226bfc4a406e06f9f1c860c3273a1d3e
Raghu K <raghuk.vit@gmail.com>
2023-03-23 08:45:50 +0530

    Update tf-module-app/

--- vars.tf
+++ vars.tf
@@ -2,4 +2,11 @@ variable "env" {}
 variable "tags" {}
 variable "component" {}
 variable "instance_type" {}
+variable "desired_capacity" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "subnets" {}
+
+
+
 
