aaff6f6d393bb8289c5099f692748423f3ff399e
Raghu K <raghuk.vit@gmail.com>
2023-03-23 08:48:06 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
+  name                = "${var.component}-${var.env}"
   desired_capacity    = var.desired_capacity
   max_size            = var.max_size
   min_size            = var.min_size
@@ -35,6 +36,11 @@ resource "aws_autoscaling_group" "main" {
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    propagate_at_launch = false
+    value               = "${var.component}-${var.env}"
   }
 }
 
