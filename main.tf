85f9824e5c1ed4f90c0a7af7c9c47a9c91ba0e55
Raghu K <raghuk.vit@gmail.com>
2023-03-24 07:47:39 +0530

    Update tf-module-app/

--- main.tf
+++ main.tf
@@ -48,29 +48,29 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
-resource "aws_security_group" "main" {
-  name        = "${var.component}-${var.env}"
-  description = "${var.component}-${var.env}"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
+//resource "aws_security_group" "main" {
+//  name        = "${var.component}-${var.env}"
+//  description = "${var.component}-${var.env}"
+//  vpc_id      = aws_vpc.main.id
+//
+//  ingress {
+//    description      = "TLS from VPC"
+//    from_port        = 443
+//    to_port          = 443
+//    protocol         = "tcp"
+//    cidr_blocks      = [aws_vpc.main.cidr_block]
+//    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+//  }
+//
+//  egress {
+//    from_port        = 0
+//    to_port          = 0
+//    protocol         = "-1"
+//    cidr_blocks      = ["0.0.0.0/0"]
+//    ipv6_cidr_blocks = ["::/0"]
+//  }
+//
+//  tags = {
+//    Name = "allow_tls"
+//  }
+//}
